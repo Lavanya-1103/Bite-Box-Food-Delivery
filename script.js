655660d5cf7759fdf4ccffc1cf8a7f4e83d769e5
Lavanya Lokhande <lokhandelavanya11@gmail.com>
2026-08-03 12:56:04 +0530

    all files upload include footer,project modification done

---
+++ script.js
@@ -0,0 +1,26 @@
+function login() {
+
+    let email=document.getElementById("email").value;
+    let password=document.getElementById("password").value;
+    let error=document.getElementById("error");
+
+    if(email==="admin@1234"  && password==="123456"){
+        localStorage.setItem("IsLoggedIn","true");
+        localStorage.setItem("user", email);
+
+        window.location.href="index.html";
+    }else{
+        error.innerText="Invalid Email or Password";
+    }
+}
+
+function togglePassword() {
+    let passwordInput = document.getElementById("password")
+
+    if (passwordInput.type === "password") {
+        passwordInput.type = "text";
+
+    }else{
+        passwordInput.type = "password";
+    }
+}
